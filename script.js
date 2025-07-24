20b11a4254297d1204ca3503ffd193b263cc6127
Sumitkumar2k7 <Sumitkumarp8292@gmail.com>
2025-07-24 22:56:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function submitQuiz() {
+  // You can extend this to check answers and calculate score
+  alert("Quiz Submitted! Redirecting to result page...");
+  window.location.href = "result.html";
+}
